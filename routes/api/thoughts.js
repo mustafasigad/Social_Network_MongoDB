1563753491c5b0f6e74634619fad9230c81f3447
Mustafa <janodones@gmail.com>
2024-03-07 00:42:56 +0300

    readme updated

--- routes/api/thoughts.js
+++ routes/api/thoughts.js
@@ -16,7 +16,7 @@ router.route('/').get(getallThoughs).post(createThought);
 router.route('/many').delete(deleteAllThoughts)
 
 //api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThough).delete( deleteThought).put(updateThought)
+router.route('/:thoughtId').get(getSingleThough).delete(deleteThought).put(updateThought)
 
 //api/thoughts/:thoughtId/reaction
 router.route('/:thoughtId/reaction').post(createReaction);
